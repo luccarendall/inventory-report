ce9f20784a5b8179a41c6ea95c5ea5a478f1e7d0
LuccaRendall <luccarendall2@gmail.com>
2023-03-07 12:01:23 -0300

    Feat:  teste da classe Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,29 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    # Testa se é possível criar um produto com os dados corretos
+    id = 1
+    nome_do_produto = "Arroz"
+    nome_da_empresa = "Marca X"
+    data_de_fabricacao = "01/01/2022"
+    data_de_validade = "01/01/2024"
+    numero_de_serie = "12345"
+    instrucoes_de_armazenamento = "em local seco e arejado"
+    # linha de respiro
+    produto = Product(
+        id,
+        nome_do_produto,
+        nome_da_empresa,
+        data_de_fabricacao,
+        data_de_validade,
+        numero_de_serie,
+        instrucoes_de_armazenamento,
+    )
+    assert produto.id == id
+    assert produto.nome_do_produto == nome_do_produto
+    assert produto.nome_da_empresa == nome_da_empresa
+    assert produto.data_de_fabricacao == str(data_de_fabricacao)
+    assert produto.data_de_validade == str(data_de_validade)
+    assert produto.numero_de_serie == numero_de_serie
+    assert produto.instrucoes_de_armazenamento == instrucoes_de_armazenamento
