e7b121b25d5c33c872d155f1f47ba2f1f78813cb
LuccaRendall <luccarendall2@gmail.com>
2023-03-11 05:29:08 -0300

    Feat: Import_data agora funciona também com arquivos em JSON

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,18 +1,30 @@
 import csv
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
+import json
 
 
 class Inventory:
-    def obtem_dados_do_arquivo(file):
-        tabela_de_dados = csv.DictReader(file)
-        dados_obtidos = [linha for linha in tabela_de_dados]
-        return dados_obtidos
+    def obtem_dados_arquivo(file, formato_do_arquivo):
+        if formato_do_arquivo == "csv":
+            tabela_dados = csv.DictReader(file)
+            dados_do_arquivo = [linha for linha in tabela_dados]
+            return dados_do_arquivo
+        if formato_do_arquivo == "json":
+            return json.load(file)
+
+    def obter_extensao_arquivo(caminho_arquivo):
+        partes = caminho_arquivo.split(".")
+        if len(partes) == 1:
+            return []
+        else:
+            return partes[-1]
 
     @staticmethod
     def import_data(path, tipo_do_relatorio):
+        formato_arquivo = Inventory.obter_extensao_arquivo(path)
         with open(path, encoding="utf-8") as file:
-            inventorio = Inventory.obtem_dados_do_arquivo(file)
+            inventorio = Inventory.obtem_dados_arquivo(file, formato_arquivo)
 
         if tipo_do_relatorio == "simples":
             return SimpleReport.generate(inventorio)
